66684b268680958783824e0357ec940fb19b939c
Hubert Walczak <hubert.walczak.lexogrine@gmail.com>
2019-10-25 21:02:30 +0200

    fix export

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,5 +11,5 @@ interface SteamPath {
 }
 export declare function getSteamLibraries(steamPath: string): string[] | null;
 export declare function getSteamPath(): any;
-export default function getGamePath(gameId: number): SteamPath | null;
+export declare function getGamePath(gameId: number): SteamPath | null;
 export {};
