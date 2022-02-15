4dd98d561958a52a4cb5518a781c75c88ac9e3a9
Hubert Walczak <hubertwalczak8@gmail.com>
2022-02-15 14:01:27 +0100

    Fixed build

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 interface GamePath {
     path: string;
     name: string;
+    executable?: Promise<any>;
 }
 interface SteamPath {
     game: GamePath | null;
@@ -11,9 +12,5 @@ interface SteamPath {
 }
 export declare function getSteamLibraries(steamPath: string): string[] | null;
 export declare function getSteamPath(): string | null;
-declare type SteamPathWithExecutable = SteamPath & {
-    executable: Promise<any>;
-};
-export declare function getGamePath(gameId: number, findExecutable: false): SteamPath | null;
-export declare function getGamePath(gameId: number, findExecutable: true): SteamPathWithExecutable | null;
+export declare function getGamePath(gameId: number, findExecutable?: boolean): SteamPath | null;
 export {};
