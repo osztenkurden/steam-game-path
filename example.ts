49a5059533ece3ac86c4e37416f658a0939d1fca
Hubert <hubertwalczak8@gmail.com>
2026-09-16 21:13:53 +0200

    chore: remove unnecesary file

--- example.ts
+++
@@ -1,6 +0,0 @@
-import { getInstalledGames, getSteamLibraries, getSteamPath } from "./tsc";
-const p = getSteamPath();
-if(!p) process.exit(1);
-const x = getSteamLibraries(p);
-const y = getInstalledGames();
-console.log(x,y)
